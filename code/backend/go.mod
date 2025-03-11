8b4c3fccb08a9bc6e1ae1ce35f71b65820a5aecd
Mats Walker <walma697@gmail.com>
2025-03-11 15:22:30 +0100

    Changes to go code. Just for learning

--- code/backend/go.mod
+++ code/backend/go.mod
@@ -1,3 +1,5 @@
-module friendly/code/backend
+module backend
 
 go 1.24.1
+
+require github.com/mattn/go-sqlite3 v1.14.24
--- code/backend/main.go
+++ code/backend/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
 )
 
+func ConnectDB(path_db string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", path_db)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Piung db to see if it returns a value
+	err = db.Ping()
+	if err != nil {
+		// Case when db does not connect
+		return nil, err
+	}
+
+	return db, err
+}
+
 func main() {
-	fmt.Println("Hello, Go!")
+	db, err := ConnectDB("../database/test_db.db")
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println("Error when trying to connect to db")
+		return
+	}
+	fmt.Println("Connected to db")
 }
 
 // To run go code: go run main.go
